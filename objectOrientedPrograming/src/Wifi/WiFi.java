c393f8f0462832fbc2f13e20c0cfc9b38ccdc73f
talhalaf <34452288+talhalaf@users.noreply.github.com>
2017-12-11 18:08:25 +0200

    Update WiFi.java

--- objectOrientedPrograming/src/Wifi/WiFi.java
+++ objectOrientedPrograming/src/Wifi/WiFi.java
@@ -3,6 +3,8 @@ package Wifi;
 
 
 public class WiFi {
+	double LAT, LON, ALT;
+	String Time;
 	private String SSID;
 	private String MAC;
 	private double freq, signal;
@@ -12,6 +14,40 @@ public class WiFi {
 		 this.SSID= SSID;
 		 this.freq= freq;
 	 }
+	public WiFi(String SSID,String MAC,  double freq, double signal,double LAT,double LON,double ALT,String Time){
+ 		this.LAT = LAT;
+ 		this.LON = LON;
+ 		this.ALT = ALT;
+ 		 this.signal=signal;
+ 		 this.MAC= MAC;
+ 		 this.SSID= SSID;
+ 		 this.freq= freq;
+ 	 }
+ 	public double getLAT(){
+ 		return LAT;	
+ 	}
+ 	
+ 	public double getLON(){
+ 		return LON;	
+ 	}
+ 	
+ 	public String getTime(){
+ 		return new String(Time);	
+ 	}
+ 	
+ 	public double getFreq(){
+ 		return freq;	
+ 	}
+ 	
+ 	public double getALT(){
+ 		return ALT;	
+ 	}
+ 	public String getSSID(){
+ 		return new String(SSID);	
+ 	}
+ 	public String getMAC(){
+ 		return new String(MAC);	
+ 	}
 	 public double getSignal(){
 		 return signal;
 	 }
